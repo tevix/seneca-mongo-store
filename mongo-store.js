7c544646a117f4acdeb850ab49c7df3c7dbeb40e
Oscar Morera <olmorera@gmail.com>
2020-01-15 14:58:46 -0600

    PD-1401

--- mongo-store.js
+++ mongo-store.js
@@ -125,9 +125,10 @@ module.exports = function(opts) {
     }
 
     conf.db = conf.db || conf.name
+    var options = conf.options || {}
 
     // Connect using the URI
-    MongoClient.connect(conf.uri, function(err, client) {
+    MongoClient.connect(conf.uri, options, function(err, client) {
       if (err) {
         return seneca.die('connect', err, conf)
       }
